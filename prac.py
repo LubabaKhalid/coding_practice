af3c606497caf9d7d077401d0feabf268012f2d5
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-23 23:16:02 +0500

    added code

---
+++ prac.py
@@ -0,0 +1,26 @@
+t=int(input())
+while(t):
+    n=int(input())
+    count=0
+    arr=[]
+    k=1
+    j=2
+    m=0
+    while count<n:
+        x=j%k
+        p= j
+        n= k+1
+        p= p+1
+        y=(p)%(n)
+        if x<y and x==m:
+            arr.append(j)
+            count+=1
+            k=k+1
+            m=m+1
+            j=j+1
+
+    t = t-1
+        
+    print(arr)
+        
+
