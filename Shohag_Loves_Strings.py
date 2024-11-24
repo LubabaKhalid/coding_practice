110faf0e8998840ef034ed91e9570f0b56236ae1
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-24 19:59:29 +0500

    change code

--- Shohag_Loves_Strings.py
+++ Shohag_Loves_Strings.py
@@ -1,11 +1,11 @@
 def fun(s):
-    if len(s)>2:
-        for j in range(len(s)-3):
-            if s[j]!=s[j+1]!=s[j+2]:
-                return s[j:j+3]
-            
-    else: 
-        return -1
+    for j in range(len(s)-1):
+        if s[j]==s[j+1]:
+            return s[j:j+2]
+    for j in range(len(s)-2):
+        a=set(s[j:j+3])
+        if len(a)==3:
+            return s[j:j+3]
     return -1
 t=int(input())
 for i in range(t):
