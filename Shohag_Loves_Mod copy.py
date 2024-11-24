64adca9595f821a683a3df32549d6de36c32ba43
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-24 19:40:00 +0500

    change code

--- Shohag_Loves_Mod copy.py
+++ Shohag_Loves_Mod copy.py
@@ -1,20 +1,5 @@
 t=int(input())
 for i in range(t):
     n=int(input())
-    count=0
-    arr=[]
-    k=1
-    flag=True
-    j=2
-    while j<100 and flag:
-        if count==n:
-            flag=False
-        x=j%k
-        y=(j+1)%(k)
-        if x<y :
-            arr.append(j)
-            count+=1
-            k=k+1
-        j=j+1
-    print(arr)
-        
+    for j in range(n):
+        print(j*2+1,end=' ')
