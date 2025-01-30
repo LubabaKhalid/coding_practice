daea94b344790f4114f958ed0045023ebb2240bc
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-01-30 20:56:25 +0500

    added file

--- dict.py
+++ dict.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
 frequency = defaultdict(int)
-for char in "hello":
+for char in "lubaba":
     frequency[char] += 1
 print(frequency.values())  
 for value in frequency.values():
     print(value)
+    
+l=list(frequency.values())
+print(l[0:2])
