eaf19df02a19d11a531a278a146e9e1aa1f9ccdd
lubaba <bsdsf22a025@pucit.edu.pk>
2025-04-26 16:42:18 +0500

    added file

--- 2028A.py
+++ 2028A.py
@@ -1,30 +1,35 @@
 t=int(input())
 for _ in range(t):
-    n,a,b=map(int,input().split())
-    s=list(input())
-    m=len(s)
-    x=0
-    y=0
-    c=0
-    while c<m:
-        if s[c]=="N":
-            y=y+1
-        elif s[c]=="E":
-            x=x+1
-        elif s[c]=="W":
-            x=x-1
-        elif s[c]=="S":
-            y=y-1
-        if x==a and y==b:
+    m,a,b=map(int,input().split())
+    s=(input())*15
+    
+    if m==1:
+        if s[0]=="N" and a==0:
             print("YES")
-            break
-        elif ((x==a and y>b) or (x>a and y>b)) and (c==(m-1)):
+        elif s[0]=="E" and b==0:
+            print("YES")
+        else:
             print("NO")
-            break
-        if c==(m-1):
-            if x==0 and y==0:
-                print("NO")
+        
+    else:
+        x=0
+        y=0
+        flag=False
+        for c in range(len(s)):
+            if s[c]=="N":
+                y=y+1
+            elif s[c]=="E":
+                x=x+1
+            elif s[c]=="W":
+                x=x-1
+            elif s[c]=="S":
+                y=y-1
+            if x==a and y==b:
+                flag=True
+                print("YES")
                 break
-            c=-1
-        c=c+1
+        if not flag:
+            print("NO")
+            
+            
     
